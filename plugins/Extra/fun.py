56b5e64da134957f382fe7d07573f0eb946cdb05
devildevildev <165453257+devildevildev@users.noreply.github.com>
2024-05-29 07:49:37 +0530

    Update fun.py

--- plugins/Extra/fun.py
+++ plugins/Extra/fun.py
@@ -41,7 +41,7 @@ async def throw_dart(client, message):
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=DART_E_MOJI,
-        disable_notification=True,
+        disable_notification=False,
         reply_to_message_id=rep_mesg_id
     )
 
@@ -61,7 +61,7 @@ async def roll_dice(client, message):
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=DICE_E_MOJI,
-        disable_notification=True,
+        disable_notification=False,
         reply_to_message_id=rep_mesg_id
     )
 
@@ -80,7 +80,7 @@ async def luck_cownd(client, message):
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=TRY_YOUR_LUCK,
-        disable_notification=True,
+        disable_notification=False,
         reply_to_message_id=rep_mesg_id
     )
 
@@ -100,7 +100,7 @@ async def roll_dice(client, message):
     await client.send_dice(
         chat_id=message.chat.id,
         emoji=GOAL_E_MOJI,
-        disable_notification=True,
+        disable_notification=False,
         reply_to_message_id=rep_mesg_id
     )
 
