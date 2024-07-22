342daa6d174ce595bc1d0fcdec6fc8590e62985c
devildevildev <165453257+devildevildev@users.noreply.github.com>
2024-07-22 19:09:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/the_hell_king_updates') #
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'zipshort.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '9ea91a2c95f97bf82e4362fd37e45f3c86120644')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "20")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
